cd08909d63bbd35a2ca66d25b6a72d2f6caca58e
Prathik M <murthy.prathik@gmail.com>
2022-04-09 14:49:40 -0400

    Revert "Update App.js"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,6 @@ import "./App.css";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import 'react-phone-number-input/style.css'
-import PhoneInput from 'react-phone-number-input'
-import React, { useEffect, useState } from "react";
-
 // const axios = require('axios')
 
 function insertclick(phone_num) {
@@ -21,19 +17,14 @@ function deleteclick(phone_num) {
 function App() {
 
 
-  const [phone_value, setValue] = useState()
+
   return (
     <div className="App">
       <header className="App-header">
-      <PhoneInput id="phone"
-        placeholder="Enter phone number"
-        value={phone_value}
-        onChange={setValue}/>
-        {/* <input type="phone" id="phone" placeholder="Phone Number" /> */}
+        <input type="phone" id="phone" placeholder="Phone Number" />
         <Row>
           <Col>
-            <Button variant="primary" onClick={() => insertclick(phone_value)}>Insert</Button>
-            {/* <Button variant="primary" onClick={() => insertclick(document.getElementById("phone").value)}>Insert</Button> */}
+            <Button variant="primary" onClick={() => insertclick(document.getElementById("phone").value)}>Insert</Button>
           </Col>
           <Col>
             <Button variant="danger" onClick={() => deleteclick(document.getElementById("phone").value)}>Delete</Button>
