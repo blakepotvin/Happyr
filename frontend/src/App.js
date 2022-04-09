72848b6641dc12b5796ba3f34e063b3ea38ae595
Blake Potvin <58959047+Prodxgy@users.noreply.github.com>
2022-04-09 09:17:49 -0400

    bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="phone" placeholder="Phone Number" />
+        <Row>
+          <Col>
+            <Button variant="primary">Insert</Button>
+          </Col>
+          <Col>
+            <Button variant="danger">Delete</Button>
+          </Col>
+        </Row>
       </header>
     </div>
   );
