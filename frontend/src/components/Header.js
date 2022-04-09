c30cb66a336784d4fe80e7d0f073bdc077b53146
Sampath Eaty <s.eaty6@gmail.com>
2022-04-09 16:42:52 -0400

    fix headerjs

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -19,9 +19,10 @@ export default class Header extends Component {
     render() {
         return (
             <StyledDiv>
-                <h1>Your Mother</h1>
-                <p id='one'>A SMS Text Service Designed for SpartaHack 7</p>
+                <h1>happyr</h1>
+                <p id='one'>A uplifitng SMS Text Service Designed for SpartaHack 7</p>
                 <p id='two'>Blake Potvin, Sampath Eaty, Prathik Murthy</p>
+                <p id='two'>Powered by: Twillio, MongoDB, and Google Cloud.</p>
             </StyledDiv>
         )
     }
