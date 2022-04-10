83d8415019f5b76e44b8ed21d7994983881b080e
Sampath Eaty <s.eaty6@gmail.com>
2022-04-10 11:30:48 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,20 @@ import Header from './components/Header.js';
 import styled from 'styled-components';
 
 // const axios = require('axios')
+// ur mom ent = require('moment') << filled in by github /copilor thing
+//i want to make a function that will take a phone number and send it to the backend
+//i want to make a function that will take a phone number and send it to the backend
+//i want to make a function that will take a phone number and send it to the backend
+//i want to make a function that will take a phone number and send it to the backend
+//i want to make a function that will take a phone number and send it to the backend
+//i want to make a function that will take a phone number and send it to the backend
+//i want to make a function that will take a phone number and send it to the backend
+//i want to make a function that will take a phone number and send it to the backend
+//i want to make a function that will take a phone number and send it to the backend
 
+//create a function that will take a phone number and send it to the backend
+//create a function that will take a phone number and send it to the backend
+//create a function that will take a phone number and send it to the backend
 function insertclick(phone_num) {
   console.log(phone_num);
   phone_num = phone_num.slice(1);
